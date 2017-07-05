6b33039337f8304c442db83eecb71f5b9916b47e
Andres Antista <andresantista@umpquabank.com>
2017-07-05 09:55:39 -0700

    add multiple dialogs example

---
+++ ex3-multiple-dialogs/script.js
@@ -0,0 +1,81 @@
+document.addEventListener("DOMContentLoaded", pageLoaded);
+
+function pageLoaded(event) {
+    document.querySelectorAll(".js-dialog-trigger").forEach(item => item.addEventListener('click', openDialog));
+}
+
+
+const KEYCODE = {
+    ESC: 27
+}
+
+let previousActiveElement;
+
+function openDialog() {
+    // Grab a reference to the previos element.
+    // We'll want to restore this when we close the dialog.
+    previousActiveElement = document.activeElement;
+
+    const target = previousActiveElement.dataset.dialogTarget;
+    const dialogSelector = `.dialog[data-dialog='${target}']`;
+    const dialog = document.querySelector(dialogSelector);
+    const dialogMask = dialog.querySelector('.dialog__mask');
+    const dialogWindow = dialog.querySelector('.dialog__window');
+
+    // Quick and dirty way to make all the siblings of our dialog inert.
+    Array.from(document.body.children).forEach(child => {
+        if (child !== dialog) {
+            child.inert = true;
+        }
+    })
+
+    // Make the dialog visible
+    dialog.classList.add('opened');
+
+    // Listen for things that should close the dialog.
+    dialogMask.addEventListener('click', closeDialog)
+    
+    // This is just for demo purposes, you'll probably want to do other actions here.
+    dialogWindow.querySelectorAll('button').forEach(btn => {
+        btn.addEventListener('click', closeDialog);
+
+    });
+    document.addEventListener('keydown', checkCloseDialog)
+
+    // Finally, move focus into the dialog.
+    dialog.querySelector('button').focus();
+}
+
+function checkCloseDialog(e) {
+    if (e.keyCode === KEYCODE.ESC)
+        closeDialog();
+}
+
+function closeDialog() {
+    // Quickly get dialog selectors. Good candidate to refactor
+    const target = previousActiveElement.dataset.dialogTarget;
+    const dialogSelector = `.dialog[data-dialog="${target}"]`;
+    const dialog = document.querySelector(dialogSelector);
+    const dialogMask = dialog.querySelector('.dialog__mask');
+    const dialogWindow = dialog.querySelector('.dialog__window');
+
+    // Clean up any event listeners.
+    dialogMask.removeEventListener('click', closeDialog);
+    dialogWindow.querySelectorAll('button').forEach(btn => {
+        btn.removeEventListener('click', closeDialog);
+    });
+    document.removeEventListener('keydown', checkCloseDialog);
+
+    // Uninert our siblings.
+    Array.from(document.body.children).forEach(child => {
+        if (child !== dialog) {
+            child.inert = false;
+        }
+    })
+
+    // Hide the dialog.
+    dialog.classList.remove('opened');
+
+    // Restore focus to the previous active element.
+    previousActiveElement.focus();
+}
